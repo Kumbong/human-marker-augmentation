ee7c95adc5b1f9378d0006ee1179f692568d2d14
Antoine Falisse <antoinefalisse@gmail.com>
2022-11-07 16:47:00 -0800

    add test

--- mySettings.py
+++ mySettings.py
@@ -22,22 +22,28 @@ def get_dense_settings(a):
 # LSTM.
 def get_lstm_settings(a):
 
-    settings = {"0":                
-                {'augmenter_type': 'lowerExtremity',
-                 "poseDetector": 'OpenPose',
-                 "idxDatasets": [idx for idx in range(0,1)],
-                 "scaleFactors": [0.9, 0.95, 1., 1.05, 1.1],
-                 "nHUnits": 96,
-                 "nHLayers": 2,
-                 "nEpochs": 50,
-                 "batchSize": 64,
-                 "idxFold": 0,
-                 'learning_r': 5e-05,
-                 "mean_subtraction": True,
-                 "std_normalization": True,
-                 "noise_magnitude": 0.018,
-                 "noise_type": "per_timestep",
-                 'nRotations': 1,
-                 'bidirectional': False}}
+    settings = {
+        "reference":
+            {'augmenter_type': 'lowerExtremity',
+             "poseDetector": 'OpenPose',
+             "mean_subtraction": True,
+             "std_normalization": True,},
+        "0":                
+            {'augmenter_type': 'lowerExtremity',
+             "poseDetector": 'OpenPose',
+             "idxDatasets": [idx for idx in range(0,1)],
+             "scaleFactors": [0.9, 0.95, 1., 1.05, 1.1],
+             "nHUnits": 96,
+             "nHLayers": 2,
+             "nEpochs": 50,
+             "batchSize": 64,
+             "idxFold": 0,
+             'learning_r': 5e-05,
+             "mean_subtraction": True,
+             "std_normalization": True,
+             "noise_magnitude": 0.018,
+             "noise_type": "per_timestep",
+             'nRotations': 1,
+             'bidirectional': False}}
         
     return settings[str(a)]
